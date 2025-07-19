beaac6cb9f058b932c07739b0cf1486751d190f5
Winnie Wei <104879662+NineWei@users.noreply.github.com>
2025-07-18 21:17:12 -0300

    Adição do ";" script.js
    Foi adicionado o ponto e vírgula no arquivo de script.js.

--- Atv1JavaScript/script.js
+++ Atv1JavaScript/script.js
@@ -1,20 +1,20 @@
-const numero1 = parseFloat(prompt("Insira o primeiro número: "))
-const numero2 = parseFloat(prompt("Insira o segundo número: "))
+const numero1 = parseFloat(prompt("Insira o primeiro número: "));
+const numero2 = parseFloat(prompt("Insira o segundo número: "));
 
-let soma = numero1
-soma += numero2
+let soma = numero1;
+soma += numero2;
 
-let sub = numero1
-sub -= numero2
+let sub = numero1;
+sub -= numero2;
 
-let multi = numero1
-multi *= numero2
+let multi = numero1;
+multi *= numero2;
 
-let divi = numero1
-divi /= numero2
+let divi = numero1;
+divi /= numero2;
 
 console.log(`A soma é: ${soma}
 A subtração é: ${sub}
 A multiplicação é: ${multi}
 A divisão é: ${divi}`
-)
+);
